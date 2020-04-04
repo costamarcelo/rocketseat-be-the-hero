0660e7c123ff208550084c4a433dea91a2a02599
Marcelo Costa <costamarcelo.ti@gmail.com>
2020-04-04 20:26:13 -0300

    Mobile App

---
+++ mobile/src/services/api.js
@@ -0,0 +1,10 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://10.0.0.102:3333'
+});
+
+export default api;
